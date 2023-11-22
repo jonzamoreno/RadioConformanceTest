ea4bf3cf3b40cf8778971ece68c747b8c36ed262
GONZALO MORENO <gonzalo_moreno@keysight.com>
2023-11-22 18:40:21 +0100

    Repository project template including CI

---
+++ Source/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RadioConformanceTests;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Press Q to quit..");
+        while (Console.ReadKey().Key != ConsoleKey.Q);
+
+        Environment.Exit(0);
+    }
+}
